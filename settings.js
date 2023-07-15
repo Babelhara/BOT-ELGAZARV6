494554adde09f0a868e3e0c0c56fdc286c0194da
botwhatsappbot77 <139276893+botwhatsappbot77@users.noreply.github.com>
2023-07-15 09:32:04 +0300

    Update settings.js

--- settings.js
+++ settings.js
@@ -15,23 +15,23 @@ global.doc5 = 'application/pdf'
 global.doc6 = 'application/vnd.android.package-archive'
 
 //owmner v card
-global.owner = ['201098906252'] //ur owner number
-global.ownername = "ᗴᒪᘜᗩᘔᗩᖇ ᗩᒪᗯᗩᘔᗴᖇ❤️" //ur owner name
+global.owner = ['+967770125159'] //ur owner number
+global.ownername = "مطور البوت الكنج عبدالله الاسطوره❤️" //ur owner name
 global.ytname = "YT: ELGAZAR" //ur yt chanel name
 global.socialm = "GitHub: ELGAZAR" //ur github or insta name
 global.location = "Africa, Egypt, Cairo" //ur location
 
 //bot bomdy 
-global.ownernomer = "201098906252" //ur number
-global.premium = ['201098906252'] //ur premium number
-global.botname = '❤️ᗴᒪᘜᗩᘔᗩᖇ ᗷOT❤️' //ur bot name
-global.linkz = "https://youtube.com/@ABDALLAH_MOHAMED" //your theme url which will be displayed on whatsapp
+global.ownernomer = "+967770125159" //ur number
+global.premium = ['+967770125159'] //ur premium number
+global.botname = '❤️بوت عبدالله الحداد فيه جميع المميزات❤️' //ur bot name
+global.linkz = "https://t.me/Abdallah_Abduh99" //your theme url which will be displayed on whatsapp
 global.websitex = "https://telegra.ph/file/ec763cc3e7c88d0e67682.jpg" //ur website to be displayed
-global.botscript = 'https://youtube.com/@ABDALLAH_MOHAMED' //script link
+global.botscript = 'https://api.whatsapp.com/send?phone=+967770125159' //script link
 global.themeemoji = "♥" //ur theme emoji
-global.packname = "⍣الجزار الوزير يروحي⍣" //ur sticker watermark packname
-global.author = "⍣01098906252⍣" //ur sticker watermark author
-global.wm = "❤️ᗴᒪᘜᗩᘔᗩᖇ ᗷOT❤️." //ur watermark
+global.packname = "⍣عبدالله الحداد يروحي⍣" //ur sticker watermark packname
+global.author = "⍣+967770125159⍣" //ur sticker watermark author
+global.wm = "❤️اروع بوت عبدالله الحداد❤️." //ur watermark
 
 // Other
 global.sessionName = 'session'
