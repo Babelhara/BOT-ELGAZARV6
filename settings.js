f7c79d81c92c94573ab1d9f97d8376f074565f21
bot865 <111519198+bot865@users.noreply.github.com>
2023-07-21 21:37:29 +0300

    Update settings.js

--- settings.js
+++ settings.js
@@ -15,23 +15,23 @@ global.doc5 = 'application/pdf'
 global.doc6 = 'application/vnd.android.package-archive'
 
 //owmner v card
-global.owner = ['201098906252'] //ur owner number
-global.ownername = "ᗴᒪᘜᗩᘔᗩᖇ ᗩᒪᗯᗩᘔᗴᖇ❤️" //ur owner name
-global.ytname = "YT: ELGAZAR" //ur yt chanel name
-global.socialm = "GitHub: ELGAZAR" //ur github or insta name
+global.owner = ['201028085788'] //ur owner number
+global.ownername = "ELAKRAB ELYOTUBER❤️" //ur owner name
+global.ytname = "YT: ELAKRAB" //ur yt chanel name
+global.socialm = "GitHub: ELAKRAB" //ur github or insta name
 global.location = "Africa, Egypt, Cairo" //ur location
 
 //bot bomdy 
-global.ownernomer = "201098906252" //ur number
-global.premium = ['201098906252'] //ur premium number
-global.botname = '❤️ᗴᒪᘜᗩᘔᗩᖇ ᗷOT❤️' //ur bot name
-global.linkz = "https://youtube.com/@ABDALLAH_MOHAMED" //your theme url which will be displayed on whatsapp
-global.websitex = "https://telegra.ph/file/ec763cc3e7c88d0e67682.jpg" //ur website to be displayed
-global.botscript = 'https://youtube.com/@ABDALLAH_MOHAMED' //script link
+global.ownernomer = "201028085788" //ur number
+global.premium = ['201028085788'] //ur premium number
+global.botname = '❤️ELAKRAB BOT ❤️' //ur bot name
+global.linkz = "https://youtube.com/channel/UCBl4N7ClPLeJ1Hg4DWZBLFA" //your theme url which will be displayed on whatsapp
+global.websitex = "https://youtube.com/channel/UCBl4N7ClPLeJ1Hg4DWZBLFA" //ur website to be displayed
+global.botscript = 'https://youtube.com/channel/UCBl4N7ClPLeJ1Hg4DWZBLFA' //script link
 global.themeemoji = "♥" //ur theme emoji
-global.packname = "⍣الجزار الوزير يروحي⍣" //ur sticker watermark packname
-global.author = "⍣01098906252⍣" //ur sticker watermark author
-global.wm = "❤️ᗴᒪᘜᗩᘔᗩᖇ ᗷOT❤️." //ur watermark
+global.packname = "⍣العقرب ي قلبي ❤⍣" //ur sticker watermark packname
+global.author = "⍣01028085788⍣" //ur sticker watermark author
+global.wm = "❤️ELAKRAB BOT ❤️." //ur watermark
 
 // Other
 global.sessionName = 'session'
