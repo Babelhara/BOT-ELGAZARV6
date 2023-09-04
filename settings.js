6f51ffc920d9e9262a2adc98086e9477849d38bc
HamzabAmirni1 <144018908+HamzabAmirni1@users.noreply.github.com>
2023-09-04 12:18:30 +0100

    Update settings.js

--- settings.js
+++ settings.js
@@ -15,23 +15,23 @@ global.doc5 = 'application/pdf'
 global.doc6 = 'application/vnd.android.package-archive'
 
 //owmner v card
-global.owner = ['201098906252'] //ur owner number
-global.ownername = "ᗴᒪᘜᗩᘔᗩᖇ ᗩᒪᗯᗩᘔᗴᖇ❤️" //ur owner name
-global.ytname = "YT: ELGAZAR" //ur yt chanel name
-global.socialm = "GitHub: ELGAZAR" //ur github or insta name
+global.owner = ['212624855939'] //ur owner number
+global.ownername = "Hamza Amirni ❤️" //ur owner name
+global.ytname = "YT: من اجمل المعرفة" //ur yt chanel name
+global.socialm = "GitHub: Hamza" //ur github or insta name
 global.location = "Africa, Egypt, Cairo" //ur location
 
 //bot bomdy 
-global.ownernomer = "201098906252" //ur number
-global.premium = ['201098906252'] //ur premium number
-global.botname = '❤️ᗴᒪᘜᗩᘔᗩᖇ ᗷOT❤️' //ur bot name
-global.linkz = "https://youtube.com/@ABDALLAH_MOHAMED" //your theme url which will be displayed on whatsapp
-global.websitex = "https://telegra.ph/file/ec763cc3e7c88d0e67682.jpg" //ur website to be displayed
-global.botscript = 'https://youtube.com/@ABDALLAH_MOHAMED' //script link
+global.ownernomer = "212624855939" //ur number
+global.premium = ['212624855939'] //ur premium number
+global.botname = '❤️Amirni ᗷOT❤️' //ur bot name
+global.linkz = "https://youtube.com/@haker_free_fire?feature=share7" //your theme url which will be displayed on whatsapp
+global.websitex = "https://telegra.ph/file/212624855939.jpg" //ur website to be displayed
+global.botscript = 'https://youtube.com/@Hamzapro-zx4xe' //script link
 global.themeemoji = "♥" //ur theme emoji
-global.packname = "⍣الجزار الوزير يروحي⍣" //ur sticker watermark packname
+global.packname = "⍣الجزار  حمزة⍣" //ur sticker watermark packname
 global.author = "⍣01098906252⍣" //ur sticker watermark author
-global.wm = "❤️ᗴᒪᘜᗩᘔᗩᖇ ᗷOT❤️." //ur watermark
+global.wm = "❤️Amirni ᗷOT❤️." //ur watermark
 
 // Other
 global.sessionName = 'session'
@@ -40,7 +40,7 @@ global.sp = ''
 
 //messages
 global.mess = {
-    success: 'تم يروحي✨',
+    success:『 تم يا صديقتي 』'
     admin: '『 الامر دا لادمن بس! 』',
     botAdmin: '『 ارفع البوت ادمن الاول 』',
     premime: '『 ميزات خاصة متميزة إذا كنت ترغب في تسجيل نوع الإيجار 』',
