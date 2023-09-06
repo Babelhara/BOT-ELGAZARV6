6439334c22de381783b8dc1bc5011d276b12b213
STROKS2004 <97605335+STROKS2004@users.noreply.github.com>
2023-09-06 23:15:50 +0300

    Update settings.js

--- settings.js
+++ settings.js
@@ -15,23 +15,23 @@ global.doc5 = 'application/pdf'
 global.doc6 = 'application/vnd.android.package-archive'
 
 //owmner v card
-global.owner = ['201098906252'] //ur owner number
-global.ownername = "ᗴᒪᘜᗩᘔᗩᖇ ᗩᒪᗯᗩᘔᗴᖇ❤️" //ur owner name
-global.ytname = "YT: ELGAZAR" //ur yt chanel name
-global.socialm = "GitHub: ELGAZAR" //ur github or insta name
-global.location = "Africa, Egypt, Cairo" //ur location
+global.owner = ['201006249542'] //ur owner number
+global.ownername = "𝑬𝑴𝑴𝑶 𝑩𝑶𝑻🖤✨" //ur owner name
+global.ytname = "xx__0___0__xx" //ur yt chanel name
+global.socialm = "xx__0___0__xx" //ur github or insta name
+global.location = "Africa, Egypt, elmansoura" //ur location
 
 //bot bomdy 
-global.ownernomer = "201098906252" //ur number
-global.premium = ['201098906252'] //ur premium number
-global.botname = '❤️ᗴᒪᘜᗩᘔᗩᖇ ᗷOT❤️' //ur bot name
-global.linkz = "https://youtube.com/@ABDALLAH_MOHAMED" //your theme url which will be displayed on whatsapp
-global.websitex = "https://telegra.ph/file/ec763cc3e7c88d0e67682.jpg" //ur website to be displayed
-global.botscript = 'https://youtube.com/@ABDALLAH_MOHAMED' //script link
-global.themeemoji = "♥" //ur theme emoji
-global.packname = "⍣الجزار الوزير يروحي⍣" //ur sticker watermark packname
-global.author = "⍣01098906252⍣" //ur sticker watermark author
-global.wm = "❤️ᗴᒪᘜᗩᘔᗩᖇ ᗷOT❤️." //ur watermark
+global.ownernomer = "201006249542" //ur number
+global.premium = ['201006249542'] //ur premium number
+global.botname = '𝑬𝑴𝑴𝑶 𝑩𝑶𝑻🖤✨' //ur bot name
+global.linkz = "https://instagram.com/xx__0___0__xx" //your theme url which will be displayed on whatsapp
+global.websitex = "https://telegra.ph/XX-0-0-XX-09-06" //ur website to be displayed
+global.botscript = 'https://instagram.com/xx__0___0__xx' //script link
+global.themeemoji = "🖤" //ur theme emoji
+global.packname = "𝑬𝑴𝑴𝑶 𝑩𝑶𝑻🖤✨" //ur sticker watermark packname
+global.author = "𝑬𝑴𝑴𝑶 𝑩𝑶𝑻🖤✨" //ur sticker watermark author
+global.wm = "𝑬𝑴𝑴𝑶 𝑩𝑶𝑻🖤✨" //ur watermark
 
 // Other
 global.sessionName = 'session'
