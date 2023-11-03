e29992c8dd9169dcae8c9d6c7bd24c28778c7c68
abdoexodia <149756623+abdoexodia@users.noreply.github.com>
2023-11-03 08:55:04 +0200

    Update settings.js
    abdoo

--- settings.js
+++ settings.js
@@ -22,16 +22,16 @@ global.socialm = "GitHub: ELGAZAR" //ur github or insta name
 global.location = "Africa, Egypt, Cairo" //ur location
 
 //bot bomdy 
-global.ownernomer = "201098906252" //ur number
-global.premium = ['201098906252'] //ur premium number
-global.botname = '❤️ᗴᒪᘜᗩᘔᗩᖇ ᗷOT❤️' //ur bot name
-global.linkz = "https://youtube.com/@ABDALLAH_MOHAMED" //your theme url which will be displayed on whatsapp
-global.websitex = "https://telegra.ph/file/ec763cc3e7c88d0e67682.jpg" //ur website to be displayed
-global.botscript = 'https://youtube.com/@ABDALLAH_MOHAMED' //script link
+global.ownernomer = "201229145227" //ur number
+global.premium = ['201229145227'] //ur premium number
+global.botname = '❤️Sasuke ᗷOT❤️' //ur bot name
+global.linkz = "https://yoALLAH_ED" //your theme url which will be displayed on whatsapp
+global.websitex = "https:c8.jpg" //ur website to be displayed
+global.botscript = 'https://youtube.coD' //script link
 global.themeemoji = "♥" //ur theme emoji
 global.packname = "⍣الجزار الوزير يروحي⍣" //ur sticker watermark packname
-global.author = "⍣01098906252⍣" //ur sticker watermark author
-global.wm = "❤️ᗴᒪᘜᗩᘔᗩᖇ ᗷOT❤️." //ur watermark
+global.author = "⍣01229145227⍣" //ur sticker watermark author
+global.wm = "❤️Sasuke ᗷOT❤️." //ur watermark
 
 // Other
 global.sessionName = 'session'
